8561070e2120e68135e4004ab4dd069d7f5211fb
Sanjami Malhotra <sanjamimalhotra@gmail.com>
2025-07-04 15:19:18 +0530

    initial commit

--- new.cpp
+++ new.cpp
@@ -12,18 +12,44 @@
 //     return 0;
 // }
 
-#include<iostream>
-using namespace std;
-int main(){
-    int x;
-    cin>>x;
-    for(int i=2;i<=x/2;i++){
-        if(x%i==0){
-            cout<<"Composite";
-        }
-        else{
-            cout<<"Prime";
-        }
-    }
-    return 0;
-}
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int x;
+//     cin>>x;
+//     for(int i=2;i<=x/2;i++){
+//         if(x%i==0){
+//             cout<<"Composite";
+//         }
+//         else{
+//             cout<<"Prime";
+//         }
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b;
+//     cin>>a>>b;
+//     int numbers;
+//     cin>>numbers;
+//     switch(numbers){
+//         case 1:
+//         cout<<a+b;
+//         break;
+//         case 2:
+//         cout<<a-b;
+//         break;
+//         case 3:
+//         cout<<a*b;
+//         break;
+//         case 4:
+//         cout<<a/b;
+//         break;
+
+//     }
+//     return 0;
+// }
+
