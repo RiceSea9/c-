e128f895fcb83c77f7d49172e08d8aecd772d66e
Sanjami <sanjamimalhotra@gmail.com>
2025-07-09 15:25:46 +0530

    Create loops.cpp

---
+++ loops.cpp
@@ -0,0 +1,72 @@
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     for(int i=1;i<=a;i++){
+//       cout<<i;
+//     }
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     for(int i=1;i<=10;i++){
+//       cout<<(a*i)<<"\n";
+//     }
+//     return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     int fact=1;
+//    for(int i=1;i<=a;i++){
+//       fact=fact*i;
+//    }
+//    cout<<fact;
+
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     int s=0;
+//     int r=0;
+//     while(a>0){
+//       s=a%10;
+//       r=s+r;
+//       a=a/10;
+//     }
+//     cout<<r;
+//     return 0;
+// }
+
+
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     int s,r;
+//     while(a>0){
+//       s=a%10;
+//       cout<<s;
+//       a=a/10;
+//     }
+//     return 0;
+// }
