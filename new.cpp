0e99ad914e1933ae5de0a5e5aa6209c0e56c1772
Sanjami <sanjamimalhotra@gmail.com>
2025-07-09 14:22:30 +0530

    Update new.cpp

--- new.cpp
+++ new.cpp
@@ -53,3 +53,119 @@
 //     return 0;
 // }
 
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     cout<<a;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     char gender;
+//     cin>>a;
+//     if(a>=18 && gender=='F'){
+//         cout<<"Female Adult";
+//     }
+//     else if(a>=18 && gender=='M')
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a,b,c;
+//     cin>>a>>b>>c;
+//     if(a>b){
+//         if(a>c){
+//             cout<<a;
+//         }
+//         else{
+//             cout<<c;
+//         }
+//     }
+//     else if(b>a){
+//         if(b>c){
+//             cout<<b;
+//         }
+//         else{
+//             cout<<c;
+//         }
+//     }
+//     else{
+//         cout<<c;
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     if((a%4==0 && a%100!=0) || a%400==0 ){
+//         cout<<"Leap year";
+//     }
+//     else{
+//         cout<<"Not a leap year";
+//     }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int a;
+//     cin>>a;
+//     if(100>=a && a>=90){
+//         cout<<"A";
+//     }
+//     else if(89>=a && a>=80){
+//         cout<<"B";
+//     }
+//     else if(79>=a && a>=70){
+//         cout<<"C";
+//     }
+//     else if(69>=a && a>=60){
+//         cout<<"D";
+//     }
+//     else if(a<60){
+//         cout<<"F";
+//     }
+    
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char a;
+//     cin>>a;
+//     if(a=='a' || a=='e' || a=='i' || a=='o' || a=='u' || a=='A' || a=='E' || a=='I' || a=='O' || a=='U'){
+//         cout<<"Vowel";
+//     }
+//     else{
+//         cout<<"Consonant";
+//     }
+//     return 0;
+// }
+
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     char a;
+//     cin>>a;
+//     if((a>='A' && a<='Z')||( a>='a' && a<='z')){
+//         cout<<"Alphabet";   
+//      }
+//      else if(a>='0' && a<='9'){
+//         cout<<"Digit";
+//      }
+//      else{
+//         cout<<"Special character";
+//      }
+
+//     return 0;
+// }
